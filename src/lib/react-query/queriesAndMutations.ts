8b1fb566842e40c49447a182e0a8e8e730c52c51
Lakshya Goel <lgoel340@gmail.com>
2024-06-26 22:13:07 +0530

    Social Media App

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -148,20 +148,19 @@ export const useDeletePost = () => {
 };
 
 export const useGetPosts = () => {
-        return useInfiniteQuery({
-            queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-            queryFn: getInfinitePosts as any,
-            getNextPageParam: (lastPage: any) => {
-                if (lastPage && lastPage.documents.length === 0) {
-                    return null;
-                }
-                const lastId = lastPage.documents[lastPage.documents.length -1 ].$id;
-                return lastId;
-            },
-            initialPageParam: 1,
-        }
-    );
-}
+    return useInfiniteQuery({
+        queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+        queryFn: getInfinitePosts as any,
+        getNextPageParam: (lastPage: any) => {
+            if (lastPage && lastPage.documents.length === 0) {
+              return null;
+            }
+            const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
+            return lastId;
+        },
+        initialPageParam: null,
+    });
+};
 
 export const useSearchPosts = (searchTerm: string) => {
     return useQuery({
